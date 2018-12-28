c26d719476bf962ea0db933b0b1413e0160d6bdc
Anwesh43 <anweshthecool0@gmail.com>
2018-12-28 14:26:30 +0530

    adding AVRNode class

--- AdjacentVerticalRotLinesStage.ts
+++ AdjacentVerticalRotLinesStage.ts
@@ -121,3 +121,46 @@ class Animator {
         }
     }
 }
+
+class AVRNode {
+    prev : AVRNode
+    next : AVRNode
+    state : State = new State()
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new AVRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawAVRNode(context)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : AVRNode {
+        var curr : AVRNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this 
+    }
+}
